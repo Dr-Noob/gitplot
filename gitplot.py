ee20ff357f058aa6624687aa9fdc8eaca58d4798
Dr-Noob <peibolms@gmail.com>
2021-12-28 16:27:32 +0100

    Add C++ support. Change C/C++ display names. Remove useless tabs and spaces

--- gitplot.py
+++ gitplot.py
@@ -15,15 +15,15 @@ def file_len(fname):
 
 def loc_count(dir, extension):
     len=0
-    
+
     # r=root, d=directories, f = files
     for r, d, f in os.walk(dir):
         for file in f:
             if file.endswith(extension):
                 file_path = os.path.join(r, file)
                 len += file_len(file_path)
-                               
-    return len            
+
+    return len
 
 ##############################
 # 0. Basic checks
@@ -38,7 +38,7 @@ if (proc.returncode != 0):
 if(len(sys.argv) != 2):
     print("Usage: " + sys.argv[0] + " GIT_URL")
     sys.exit(1)
-    
+
 regex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
@@ -51,14 +51,14 @@ url=sys.argv[1]
 if(re.match(regex, url) is None):
     print('ERROR: ' + url + ' is not a valid URL')
     sys.exit(1)
-    
+
 ##############################
-# 1. Download repo    
-dir=os.path.basename(url)  
+# 1. Download repo
+dir=os.path.basename(url)
 
 if(os.path.exists(dir)):
     print('WARNING: Directory ' + dir + ' already exists. Skipping git clone')
-else:    
+else:
   proc = subprocess.Popen(["/usr/bin/git", "clone", url], stdout=subprocess.PIPE)
   proc.communicate()
 
@@ -68,12 +68,12 @@ else:
 
 os.chdir(dir)
 proc = subprocess.Popen(["/usr/bin/git", "rev-list", "--remotes"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-(commits, err) = proc.communicate()    
+(commits, err) = proc.communicate()
 
 ##############################
-# 2. Init language structures lines        
-lang_ext = [".c", ".h", "Makefile", ".md", ".py", ".tex", ".sh"]
-lang_name = [".c", ".h", "Makefile", "Markdown", "Python", "LaTeX", "Shell"]
+# 2. Init language structures lines
+lang_ext = [".c", ".h", ".cpp", ".hpp", "Makefile", ".md", ".py", ".tex", ".sh"]
+lang_name = ["C", "C headers", "C++", "C++ headers", "Makefile", "Markdown", "Python", "LaTeX", "Shell"]
 assert(len(lang_ext) == len(lang_name))
 
 N_LANGUAGES = len(lang_name)
@@ -85,22 +85,22 @@ for i in range(0, N_LANGUAGES):
 ##############################
 # 3. Count lines
 
-for c in commits.splitlines():     
+for c in commits.splitlines():
     proc = subprocess.Popen(["/usr/bin/git", "checkout", c], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    proc.communicate()        
-    
+    proc.communicate()
+
     for i in range(0, N_LANGUAGES):
         loc_list[i].append(loc_count(".", lang_ext[i]))
 
 ##############################
-# 4. Flip the lists and plot the results                           
+# 4. Flip the lists and plot the results
 for i in range(0, N_LANGUAGES):
-    loc_list[i] = np.asarray(loc_list[i])                           
+    loc_list[i] = np.asarray(loc_list[i])
     loc_list[i] = np.flip(loc_list[i])
 
 # Any list works, as we only need the length
 x = np.arange(0, np.size(loc_list[0]))
-    
+
 for i in range(0, N_LANGUAGES):
     if any(loc_list[i]):
         plt.plot(x, loc_list[i], label=lang_name[i])
@@ -111,7 +111,4 @@ plt.xlabel("Commit number")
 plt.ylabel("Lines of Code")
 plt.title(dir + ' repository')
 plt.legend()
-plt.show()    
-
-    
-    
+plt.show()
